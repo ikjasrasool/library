926edfd24f36dfbacd76023c0f1f5df4ef6954d6
ikjasrasool <ikjasrasool2022@gmail.com>
2025-02-03 10:04:27 +0530

    update secure

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,15 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyANJgZs0_GUpaCq0_MHQH-2JOlI5BOJqUM",
-  authDomain: "library-49e89.firebaseapp.com",
-  projectId: "library-49e89",
-  storageBucket: "library-49e89.appspot.com",
-  messagingSenderId: "960454078285",
-  appId: "1:960454078285:web:5e16f28b2e8f0fb9d19104",
-  measurementId: "G-QC1K2XXP0T"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -21,6 +19,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Export the auth and db instances for use in other parts of the app
 export { auth, db };
-
